e61cd6513f81288106dab8dde515315f408b6edf
root <root@cea2b93bf2f3>
2017-04-18 13:35:50 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "WavemakerSampleGJS",
+  "homePage" : "Main",
+  "name" : "WavemakerSampleGJS",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
